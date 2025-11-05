6d68474758e508c5d1b435c0a9475895980466d7
Állek Sampaio <alleksamp@gmail.com>
2025-11-05 16:13:28 -0300

    chore: Criação da injeção de dependencia na camada IoC

---
+++ AcoesInvest.Infra.IoC/AppInvest.cs
@@ -0,0 +1,36 @@
+﻿using AcoesInvest.Application.Mapper;
+using AcoesInvest.Domain.Interfaces.Repositories;
+using AcoesInvest.Infra.Data.Repositories;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AcoesInvest.Infra.IoC;
+
+public static class AppInvest
+{
+    private static void ConfigureMapper(this IServiceCollection services)
+    {
+        var mapperConfig = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new DomainToViewModelProfile());
+        });
+
+        services.AddSingleton(mapperConfig.CreateMapper());
+    }
+
+    public static IServiceCollection ConfigureDependencies(this IServiceCollection services)
+    {
+        services.ConfigureMapper();
+        return services;
+    }
+
+    public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAcoesRepository, AcoesRepository>();
+        services.AddScoped<IAcoesService, AcoesService>();
+        services.AddScoped<IAcoesAppService, AcoesAppService>();
+
+        return services;
+    }
+
+}
